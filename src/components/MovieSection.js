24b60f559bf603ccc0c252f240571ac068c1917e
Raivals <103427644+Raivals@users.noreply.github.com>
2022-11-25 16:29:05 +0100

    End project

--- src/components/MovieSection.js
+++ src/components/MovieSection.js
@@ -5,6 +5,38 @@ function MovieSection({ title, items }) {
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow--left">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 19.5L8.25 12l7.5-7.5"
+          />
+        </svg>
+      </div>
+      <div className="movieRow--right">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M8.25 4.5l7.5 7.5-7.5 7.5"
+          />
+        </svg>
+      </div>
       <div className="movieRow--listarea">
         <div className="movieRow--list">
           {items.results.length > 0 &&
